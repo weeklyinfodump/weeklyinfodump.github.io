31937590d887249c95932808c7bd2430ee11b07a
Gemcutter <spud.hjc@gmail.com>
2024-10-21 23:53:48 +1100

    update 1.0.1

--- posts.js
+++ posts.js
@@ -27,19 +27,60 @@ $("#previousWeeks").ready(function() {
         posts = sortPosts(posts, $('#sort').val())
         loadPosts()
     })
+    $("#search").on("change", function() {
+        loadPosts()
+    })
 })
 
 function loadPosts() {
+    let tally = 0;
+    search = $("#search").val();
+    searchTerms = search.toLowerCase().split(" ")
     document.getElementById("previousWeeks").innerHTML = ''
+    tmp = ""
+    tmp2 = ""
     for (let i in posts) {
-        let tmp = "<a class='row mb-4' href='"+posts[i].url+"'>"
-        tmp += "<span class='lead col-9'>"+posts[i].title+"</span>"
-        tmp += "<span class='col-3'>"+posts[i].date.toString()+"</span>"
-        tmp += "<span>"+posts[i].description+"</span>"
-        tmp += "</a>"
-        document.getElementById("previousWeeks").innerHTML += tmp
+        let searchFound1 = search=="";
+        let comparisonTerms = posts[i].title.toLowerCase().split(" ")
+        for (let j in searchTerms) {
+            for (let k in comparisonTerms) {
+                if (searchTerms[j]==comparisonTerms[k]) {
+                    searchFound1=true;
+                }
+            }
+        }
+        if (searchFound1) {
+            tally++
+            tmp += "<a class='row mb-4' href='"+posts[i].url+"'>"
+            tmp += "<span class='lead col-9'>"+posts[i].title+"</span>"
+            tmp += "<span class='col-3'>"+posts[i].date.toString()+"</span>"
+            tmp += "<span>"+posts[i].description+"</span>"
+            tmp += "</a>"
+        }
+        
+        let searchFound2 = search=="";
+        comparisonTerms = posts[i].description.toLowerCase().split(" ")
+        for (let j in searchTerms) {
+            for (let k in comparisonTerms) {
+                if (searchTerms[j]==comparisonTerms[k]) {
+                    searchFound2=true;
+                }
+            }
+        }
+        if (searchFound2&&!searchFound1) {
+            tally++
+            tmp2 += "<a class='row mb-4' href='"+posts[i].url+"'>"
+            tmp2 += "<span class='lead col-9'>"+posts[i].title+"</span>"
+            tmp2 += "<span class='col-3'>"+posts[i].date.toString()+"</span>"
+            tmp2 += "<span>"+posts[i].description+"</span>"
+            tmp2 += "</a>"
+        }
     }
-
+    document.getElementById("previousWeeks").innerHTML += tmp
+    document.getElementById("previousWeeks").innerHTML += tmp2
+    if (tally==0) {
+        tmp = "<span>There are no posts matching your search</span>"
+    }
 }
 
 function sortPosts(arr,sort) {
@@ -53,6 +94,7 @@ function sortPosts(arr,sort) {
     set2sorted = sortPosts(set2,sort)
     let tmp = []
     for (let i in arr) {
+        console.log(i)
         if (set1sorted.length==0) {
             tmp.push(set2sorted.shift())
         }
